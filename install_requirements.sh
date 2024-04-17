677dd1f15daba4a8e772c979ff1a6d1c9b1c60b4
ssanchaaa <you@example.com>
2024-04-17 16:48:16 -0400

    requirements was modified; readme.txt was added

--- install_requirements.sh
+++ install_requirements.sh
@@ -4,4 +4,6 @@ sudo apt update &&
 sudo apt install python3.9
 
 python3.9 -m pip install numpy 
-
+python3.9 -m pip install colorama 
+python3.9 -m pip install threading 
+python3.9 -m pip install regex 
